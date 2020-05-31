b03dc83da0194f603947038ebb68aa09bfc323a4
MarKain1234 <65804047+MarKain1234@users.noreply.github.com>
2020-06-01 00:00:31 +0900

    Add files via upload

---
+++ AxB.7.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+	int a,b,c;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	c=a * b;
+	printf("%d",c);
+}
---
+++ 사칙연산.9.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+	int a,b,c,d,e,f,g=0;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	
+	c =a+b;
+	d =a-b;
+	e = a*b;
+	f=a/b;
+	g=a%b;
+	
+	printf("%d\n",c);
+    printf("%d\n",d);
+    printf("%d\n",e);
+    printf("%d\n",f);
+    printf("%d\n",g);
+}
